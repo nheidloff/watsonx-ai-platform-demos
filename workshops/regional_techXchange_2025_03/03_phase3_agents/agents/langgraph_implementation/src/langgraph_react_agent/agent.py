5b7ba1a76d85b8e0289e2a51ec70710ce3491db1
Thomas Südbröcker <thomas.suedbroecker@de.ibm.com>
2025-03-17 13:33:19 +0100

    update workshop based on folder naming changes

--- workshops/regional_techXchange_2025_03/03_phase3_agents/agents/langgraph_implementation/src/langgraph_react_agent/agent.py
+++ workshops/regional_techXchange_2025_03/03_phase3_agents/agents/langgraph_implementation/src/langgraph_react_agent/agent.py
@@ -1,10 +1,12 @@
 from typing import Callable
 
 from ibm_watsonx_ai import APIClient
+from ibm_watsonx_ai.foundation_models.schema import TextGenParameters
 from langchain_ibm import ChatWatsonx
 from langgraph.graph.graph import CompiledGraph
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
+
 
 from langgraph_react_agent import TOOLS
 import logging
@@ -14,8 +16,20 @@ logging.basicConfig(level=logging.INFO)
 def get_graph_closure(client: APIClient, model_id: str) -> Callable:
     """Graph generator closure."""
 
+    parameters = TextGenParameters(
+                    decoding_method = "greedy",
+                    repetition_penalty = 1,
+                    max_new_tokens = 4000,
+                    min_new_tokens = 0,
+                    stop_sequences= [],
+                    temperature=0.0,
+                    top_p=1,
+                )
+
     # Initialise ChatWatsonx
-    chat = ChatWatsonx(model_id=model_id, watsonx_client=client)
+    chat = ChatWatsonx(model_id=model_id,  
+                       parameters= parameters, 
+                       watsonx_client=client)
 
     # Define system prompt
     default_system_prompt = """
@@ -31,7 +45,8 @@ def get_graph_closure(client: APIClient, model_id: str) -> Callable:
 
         # Create instance of compiled graph
         return create_react_agent(
-            chat, tools=TOOLS, 
+            chat, 
+            tools=TOOLS, 
             checkpointer=memory, 
             state_modifier=system_prompt
         )
